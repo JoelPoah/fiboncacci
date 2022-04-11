ddb148465441501ef533d41261d1c227131555a3
JoelPoah <89387791+JoelPoah@users.noreply.github.com>
2022-04-11 22:00:53 +0800

    first commit

---
+++ fib.py
@@ -0,0 +1,21 @@
+#this python program produces the fib of any arbitrary nth term 
+
+
+def fib(a):
+    if(a<=2):
+        return 1
+    else: 
+        return fib(a-1)+fib(a-2)
+
+
+def program():
+    a= int(input("Key in fib nth term"))
+    print(fib(a))
+    trial = str(input("Do you wanna check for another nth term?: Y/N"))
+    if trial=="N":
+        return('Do visit again!')
+        
+    else:
+        return(program())
+print(program())
+
